20c09c0851090578d7eaed58e08ce68bdc6dd863
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-04-04 16:31:03 -0500

    Fixing file address

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,12 +1,4 @@
 #Using Puppet, create a file in /tmp.
-#
-#Requirements:
-#
-#File path is /tmp/holberton
-#File permission is 0744
-#File owner is www-data
-#File group is www-data
-#File contains I love Puppet
 
 file { 'holberton':
   path    => '/tmp/holberton',
